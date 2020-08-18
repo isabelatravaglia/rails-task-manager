9946b2e51f525c3ad474a3efe2320f0d9bf498cb
isabela santos <isabelatravaglia@gmail.com>
2020-08-18 16:06:37 +0100

    added new and create routes and controllers

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,26 @@ class TasksController < ApplicationController
     def show
       @task = Task.find(params[:id])
     end
+
+    def new
+        @task = Task.new
+    end
+
+    def create
+        @task = Task.new(task_params)
+        if @task.save
+          flash[:success] = "Object successfully created"
+          redirect_to @task
+        else
+          flash[:error] = "Something went wrong"
+          render 'new'
+        end
+    end
+    
+    private
+
+    def task_params
+        params.require(:task).permit(:title, :details, :completed)
+    end
+    
 end
